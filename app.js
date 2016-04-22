f960a7204e3abe1d626e63ed90f7942ad487b5f6
mguida22 <michael.guida@colorado.edu>
2016-04-22 15:27:03 -0600

    prep for building out features

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 
 require('dotenv').config({silent: true});
 
-const request = require('request');
+const got = require('got');
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const botmon = require('./botmon');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,30 +26,26 @@ app.get('/webhook', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-  let sender, text;
+  let sender, text, msg;
 
   req.body.entry[0].messaging.forEach(evt => {
     sender = evt.sender.id;
     if (evt.message && evt.message.text) {
-      let msg = {
-        text: evt.message.text
-      }
+      msg = botmon.reply(evt.message.text);
 
-      request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: { access_token: process.env.FB_ACCESS_TOKEN },
-        method: 'POST',
-        json: {
+      got.post('https://graph.facebook.com/v2.6/me/messages', {
+        json: true,
+        headers: {
+          'Content-type': 'application/json'
+        },
+        query: { access_token: process.env.FB_ACCESS_TOKEN },
+        body: JSON.stringify({
           recipient: { id: sender },
           message: msg,
-        }
-      }, (err, res, body) => {
-        if (err) {
-          console.log('Error sending message: ', err);
-        } else if (res.body.error) {
-          console.log('Error: ', res.body.error);
-        }
-      });
+        })
+      }).catch(err => {
+        console.error(err.response.body);
+      })
     }
   });
 
---
+++ botmon.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const got = require('got');
+
+function reply(text) {
+  // current trending news
+  // where should i ski? (most snow)
+  // current weather for your location
+  // weather for any location
+  // fortune cookie style fortune
+  // make a random decision
+  // random jokes
+  // new movies
+  // movie reviews
+  // movie trailers
+  // movie showtimes
+
+  return {
+    text
+  };
+}
+
+function news(text) {
+  return text;
+}
+
+function ski(text) {
+  return text;
+}
+
+function weather(text) {
+  return text;
+}
+
+function fortunes(text) {
+  return text;
+}
+
+function decision(text) {
+  return text;
+}
+
+function jokes(text) {
+  return text;
+}
+
+function movie(text) {
+  return text;
+}
+
+module.exports = {
+  reply
+}
